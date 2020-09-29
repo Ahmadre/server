6e43b987697a17f57206c356d1e26baeddb655a7
Ahmadre <rebar.ahmad@telexiom.de>
2020-09-30 00:32:41 +0200

    Initial Commit

---
+++ src/main/java/com/telexiom/ngschulung/NgSchulungApplication.java
@@ -0,0 +1,13 @@
+package com.telexiom.ngschulung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgSchulungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgSchulungApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/telexiom/ngschulung/NgSchulungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telexiom.ngschulung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgSchulungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
